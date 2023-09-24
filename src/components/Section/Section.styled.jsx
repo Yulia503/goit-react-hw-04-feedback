69c04aa42bd05976deb909a84e37314fcb2f1f22
Yulia503 <125694915+Yulia503@users.noreply.github.com>
2023-09-24 23:38:38 +0300

    Update Section.styled.jsx

--- src/components/Section/Section.styled.jsx
+++ src/components/Section/Section.styled.jsx
@@ -3,6 +3,6 @@ import styled from 'styled-components';
 export const SectionWrapper = styled.section`
   display: flex;
   flex-direction: column;
-  padding-top: 20px;
+  padding-top: 19px;
   padding-bottom: 20px;
 `;
